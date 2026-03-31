0878f8735439119f5997c4570ec1242b85c3bdd2
Prajjawal Vaishya <128378315+Prajjawal-Vaishya@users.noreply.github.com>
2026-03-31 23:15:15 +0530

    Drawing mechanic work is done 70%

---
+++ Source/MaXangleXR/MXInkPlugin.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MXInkPlugin.h"
+
+MXInkPlugin::MXInkPlugin()
+{
+}
+
+MXInkPlugin::~MXInkPlugin()
+{
+}
